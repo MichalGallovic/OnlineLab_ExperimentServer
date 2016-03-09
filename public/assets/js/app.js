1a98513f59d32a722bbec8972db4b1a5f6a89f79
MichalGallovic <michalgallovic@MacBook-Pro.local>
2016-03-09 22:22:14 +0100

    Refactoring into components done

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -66,7 +66,7 @@ Vue.component('olm-graph', {
 	watch: {
 		series: function(newSeries, oldSeries) {
 			var chart = this.getjQueryGraph().highcharts();
-			
+
 			if(newSeries.length == oldSeries.length) {
 				for(var i = 0; i < newSeries.length; i++) {
 					chart.series[i].setData(newSeries[i].data)
@@ -92,25 +92,25 @@ var vm = new Vue({
 	experimentIntervalId: null,
 	experimentMeasuringRate: null,
 	ready : function() {
+		$(this.$el).show();
 		this.getDevices();
-		this.showExperiments();
+		this.showExperiments();		
 	},
 	data : {
 		experiments : null,
 		devices: null,
 		activeDevice: null,
-		activeMenu: "last_experiment",
+		activeMenu: "info",
 		activeExperiment: null,
 		experimentTypes: null,
 		selectedExperiment: null,
-		experimentData: null,
+		experimentData: [{data:[]}],
 		experimentsHistory : [],
 		pastExperiment: {
 			series: [{data:[]}],
 			id: null,
 			description: null
-		},
-		outputArguments: null
+		}
 	},
 	methods : {
 		runExperiment: function(event) {
@@ -127,31 +127,49 @@ var vm = new Vue({
 				data: formData
 			});
 
-			this.experimentIntervalId = setInterval(this.readExperimentData, 500);
+			this.startListening();
 					
+		},
+		startListening: function() {
+			this.experimentIntervalId = setInterval(this.readExperimentData, 500);
 		},
 		readExperimentData: function() {
 			var me = this;
 			$.getJSON('api/devices/' + this.activeDevice.id + '/readexperiment')
 			 .done(function(response) {
-			 	var experimentData = me.formatChartInput(response.data);
-			 	// console.log(experimentData);
-			 	if(!me.experimentData && experimentData && experimentData.length > 0) {
-			 		me.initGraph(experimentData);
-			 		me.experimentData = experimentData;
-			 		me.experimentMeasuringRate = response.measuring_rate;
-			 		if(!me.experimentIntervalId) {
-			 			me.experimentIntervalId = setInterval(me.readExperimentData, 500);
-			 		}
-			 	}
+			 	me.experimentData = me.formatGraphInput(
+			 		response.data, 
+			 		response.measuring_rate,
+			 		me.activeExperiment.output);
 
-			 	if(me.experimentData) me.experimentData = experimentData;
 			 })
 			 .fail(function(response) {
 			 	if(me.experimentData) {
 			 		clearInterval(me.experimentIntervalId);
 			 	}
 			 });
+		},
+		getDeviceStatus: function(id) {
+			return $.getJSON('api/devices/' + id);
+		},
+		isRunningExperiment: function() {
+			var me = this;
+			$.each(this.devices, function(index, device) {
+				me.getDeviceStatus(device.id)
+				  .done(function(response) {
+				  	if(response.status == "experimenting") {
+				  		me.pickDevice(device);
+				  		me.startListening();
+				  		noty ({
+				  			text : "Device " + device.name + " is running experiment!",
+				  			theme: "relax",
+				  			layout: "topRight",
+				  			timeout : 5000,
+				  			type: 'information'
+				  		});
+				  	}
+				});
+			});
 		},
 		stopExperiment: function() {
 			clearInterval(this.experimentIntervalId);
@@ -172,24 +190,6 @@ var vm = new Vue({
 				"experiment_input": experiment_input
 			};
 		},
-		formatChartInput: function(data) {
-			var me = this;
-			var series = [];
-			$.each(data, function(index, measurement) {
-				var measurementWithTime = [];
-				$.each(measurement, function(index, value) {
-					measurementWithTime.push([index*me.experimentMeasuringRate, value]);
-				});
-				series.push({
-					type: "line",
-					name: me.activeExperiment.output[index],
-					data: measurementWithTime,
-					visible: false
-				});
-			});
-
-			return series;
-		},
 		formatGraphInput: function(data, rate, output_arguments) {
 			var me = this;
 			var series = [];
@@ -209,7 +209,7 @@ var vm = new Vue({
 			return series;
 		},
 		showInfo: function() {
-			this.activeMenu = "info";
+			introJs().start();
 		},
 		showExperiments: function() {
 			var me = this;
@@ -221,22 +221,13 @@ var vm = new Vue({
 					});
 				});
 		},
-		deactivateDevice: function() {
-			this.activeDevice.active = false;
-			this.activeDevice = null;
-		},
 		pickDevice: function(device) {
 			this.activeMenu = "device";
-			if(!this.activeDevice) {
-				this.activeDevice = device;
-				device.active = true;
-			}
+			this.activeDevice = device;
+			device.active = true;
+			
 			if(!this.selectedExperiment) {
 				this.selectedExperiment = 1;
-			} else {
-				// console.log(this.experimentData);
-				this.initGraph(this.experimentData);
-				// this.selectExperiment(this.selectedExperiment);
 			}
 
 		},
@@ -248,20 +239,10 @@ var vm = new Vue({
 				$.each(experiments, function(index, experiment){
 					if(experiment.id == id) {
 						me.activeExperiment = experiment;
-						me.readExperimentData();
 					}
 				});
 			}
 
-		},
-		getLatestExperiment: function() {
-			var me = this;
-
-			$.getJSON('api/experiments/latest?include=measurements')
-			 .done(function(response) {
-			 	console.log(response.data.measurements.data.measurements);
-		 		me.initGraph(response.data.measurements.data.measurements);
-			 });
 		},
 		getExperimentsHistoryForDevice: function(id) {
 			var me = this;
@@ -283,11 +264,6 @@ var vm = new Vue({
 						response.data.measurements.data.measurements_rate,
 						response.data.output_arguments.data
 					);
-
-					// me.outputArguments = response.data.output_arguments.data;
-					// me.experimentMeasuringRate = response.data.measurements.data.measurements_rate;
-					// var experimentData = me.formatChartInput2(response.data.measurements.data.measurements);
-					// me.initGraph(experimentData);
 				});
 		},
 		getExperimentDataById: function(id) {
@@ -314,46 +290,6 @@ var vm = new Vue({
 		getDevicesPromise: function() {
 			return $.getJSON('api/server/devices');
 		},
-		initGraph: function(series) {
-			var me = this;
-
-			var chart = $(".olm-graph").highcharts({
-				
-				title: {
-				    text: me.experimentDescription
-				},
-				xAxis: {
-					title: {
-						text: "Simulation time"
-					},
-					labels: {
-						formatter: function() {
-							if(this.value <= 1000) {
-								return this.value;
-							}
-
-							return this.value / 1000.00;
-						}
-					}
-				},
-				yAxis: {
-				    title: {
-				        text: 'Measurement value'
-				    }
-				},
-				legend: {
-					align: 'right',
-		            verticalAlign: 'top',
-		            layout: 'vertical',
-		            x: 0,
-		            y: 0,
-		            itemMarginTop: 10
-				},
-				series: series
-			});
-
-			console.log($(".olm-graph").highcharts());
-		}
 	},
 	computed : {
 		experimentDescription: function() {
@@ -366,15 +302,10 @@ vm.$watch('selectedExperiment', function(id) {
 	this.selectExperiment(id);
 });
 
-vm.$watch('experimentData', function(newData, oldData) {
-	var numMeasuremnetTypes = newData.length;
-	var chart = $('.olm-graph').highcharts();
+vm.$watch('activeExperiment', function() {
+	
+});
 
-	// console.log(chart);
-	if(chart) {
-		// console.log(numMeasuremnetTypes);
-		for(var i = 0; i < numMeasuremnetTypes; i++) {
-			chart.series[i].setData(newData[i].data);
-		}
-	}
+vm.$watch('devices', function() {
+	this.isRunningExperiment();
 });
