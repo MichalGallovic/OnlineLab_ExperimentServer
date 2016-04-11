0b79300d849a446a64272725a47d7a944c873d59
novotny-patrick <novotny.patrick@gmail.com>
2016-04-11 19:37:57 +0200

    ledcube added with live cam

---
+++ public/proj/.build/uno/src/sketch.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+void setup() {                
+    // initialize the digital pin as an output.
+    // Pin 13 has an LED connected on most Arduino boards:
+    pinMode(13, OUTPUT);     
+}
+
+void loop() {
+    digitalWrite(13, HIGH);   // set the LED on
+    delay(1000);              // wait for a second
+    digitalWrite(13, LOW);    // set the LED off
+    delay(1000);              // wait for a second
+}
+                
