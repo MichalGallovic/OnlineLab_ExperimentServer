cc95511ebeab9c44594867e4c8ee76cc347b2364
MichalGallovic <gallovicm@gmail.com>
2016-04-18 12:06:32 +0200

    Add experiment_commands as include in API endpoint /servers/experiments

--- app/Classes/Transformers/AvailableExperimentTransformer.php
+++ app/Classes/Transformers/AvailableExperimentTransformer.php
@@ -12,12 +12,12 @@ class AvailableExperimentTransformer extends TransformerAbstract
 
 	protected $availableIncludes = [
 		"input_arguments",
-		'output_arguments'
+		'output_arguments',
+		"experiment_commands"
 	];
 
 	public function transform(Experiment $experiment)
 	{
-
 		return [
 			"id"	=>	$experiment->id,
 			"device" 		=>	$experiment->device->type->name,
@@ -35,5 +35,12 @@ class AvailableExperimentTransformer extends TransformerAbstract
 	public function includeOutputArguments(Experiment $experiment)
 	{
 		return $this->item($experiment->getOutputArgumentsAll(), new GeneralArrayTransformer);
+	}
+
+	public function includeExperimentCommands(Experiment $experiment)
+	{
+		$commands = $experiment->getExperimentCommands();
+		$commands = !is_null($commands) ? $commands : [];
+		return $this->item($commands, new GeneralArrayTransformer);
 	}
 }
