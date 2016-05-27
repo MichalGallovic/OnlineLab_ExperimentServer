92fb797b3f08717a23b9bef674ed9ec2687d0126
MichalGallovic <michalgallovic@MacBook-Pro.local>
2016-05-27 12:20:09 +0200

    Add ledcube exps!

--- config/experiments.php
+++ config/experiments.php
@@ -3,9 +3,14 @@
 return [
 	"tos1a"	=> [
 		"openloop"	=>	["start"],
-		"matlab"	=>	["start"]
+		"matlab"	=>	["start"],
+		"scilab"	=>	["start"]
 	],
         "segway"	=> [
 		"openmodelica"	=>	["change","init","start"]
+	],
+	"led_cube"	=> [
+		"c"	=>	["change"],
+		"javascript"	=>	["change"]
 	]
 ];
