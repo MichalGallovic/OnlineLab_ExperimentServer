413ff114009fe6473b2f4369f619b312a2bce566
michalgallovic <gallovicm@gmail.com>
2016-05-29 11:15:50 +0200

    Changes

--- config/experiments.php
+++ config/experiments.php
@@ -3,7 +3,8 @@
 return [
 	"tos1a"	=> [
 		"openloop"	=>	["start"],
-		"matlab"	=>	["start"]
+		"matlab"	=>	["start"],
+		"scilab"	=>	["start"]
 	],
         "segway"	=> [
 		"openmodelica"	=>	["change","init","start"]
